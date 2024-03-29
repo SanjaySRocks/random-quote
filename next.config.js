91de8d72aceb759a665f6b7bc2334e575b9beea6
Sanjay Singh <30376256+SanjaySRocks@users.noreply.github.com>
2024-03-29 15:53:32 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,9 @@
-/** @type {import('next').NextConfig} */
-// const nextConfig = {}
-
-// module.exports = nextConfig
-
-
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   // Existing configuration options
-  assetPrefix: isProd ? '/<repository>/' : '',
-  basePath: isProd ? '/<repository>' : '',
+  assetPrefix: isProd ? '' : '',
+  basePath: isProd ? '' : '',
   // Add this line
   output: 'export',
 };
