f9ff1e399f80b1d2e00633833b99fecb5c984d14
Sanjay Singh <30376256+SanjaySRocks@users.noreply.github.com>
2024-07-06 23:50:15 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,15 +8,4 @@ module.exports = {
   basePath: isProd ? '/random-quote' : '',
   assetPrefix: isProd ? '/random-quote/' : '',
   output: 'export',
- 
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
- 
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
- 
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
-}
- 
-module.exports = nextConfig
+};
