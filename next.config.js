babc22f6a9d19f590e0e6ec3429e9c0c950f7f97
Sanjay Singh <30376256+SanjaySRocks@users.noreply.github.com>
2024-07-06 23:40:46 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,17 @@
-/** @type {import('next').NextConfig} */
-// const nextConfig = {}
-
-// module.exports = nextConfig
-
-
-const isProd = process.env.NODE_ENV === 'production';
-
-module.exports = {
-  // Existing configuration options
-  assetPrefix: isProd ? '/<repository>/' : '',
-  basePath: isProd ? '/<repository>' : '',
-  // Add this line
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
   output: 'export',
-};
+ 
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+ 
+module.exports = nextConfig
